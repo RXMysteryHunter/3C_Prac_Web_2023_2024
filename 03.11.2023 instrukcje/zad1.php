1c828a108df90e3ab93511d24861795646ead371
MysteryHunter <68793039+RXMysteryHunter@users.noreply.github.com>
2023-11-17 10:14:13 +0100

    Add files via upload

---
+++ 03.11.2023 instrukcje/zad1.php
@@ -0,0 +1,15 @@
+<?php
+$liczba = -5;
+
+if ($liczba > 0) {
+    echo "Liczba jest dodatnia";
+}
+
+if ($liczba == 0) {
+    echo "Liczba jest równa 0";
+}
+
+if ($liczba < 0) {
+    echo "Liczba jest ujemna";
+}
+?>
---
+++ 03.11.2023 instrukcje/zad2.php
@@ -0,0 +1,13 @@
+<?php
+$liczba = 5;
+
+if ($liczba % 2 == 0) {
+    echo "Liczba jest parzysta";
+} else {
+    echo "Liczba jest nieparzysta";
+}
+
+if ($liczba <= 0) {
+    echo "Zła liczba";
+}
+?>
